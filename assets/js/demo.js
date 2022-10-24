6c3ea0608663526640a9d0cb17fb034cda100b2b
MedMK <maknimohamed@isimsf.u-sfax.tn>
2022-10-24 23:45:07 +0200

    a

---
+++ assets/js/demo.js
@@ -0,0 +1,7 @@
+$('.button').click(function(){
+    var buttonId = $(this).attr('id');
+    $('#modal-container').removeAttr('class').addClass(buttonId);
+    $('body').addClass('modal-active');
+  })
+  
+  
